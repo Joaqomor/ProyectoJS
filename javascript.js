2bdeecb18d1af3cb30b332431bd366e1da4916b4
Joaquin Morillo <morillojoaquin@hotmail.com>
2022-06-11 18:32:56 -0300

    commit inicial

---
+++ javascript.js
@@ -0,0 +1,30 @@
+/* 
+debugger
+var material = prompt("Ingrese el tipo de material que desea sacar del inventario");
+var cantidad = parseInt(prompt("ingrese la cantidad que desea"));
+const stock = 1365;
+var resul = stock - cantidad ;
+
+if (resul > 0) {
+
+    alert("se van a descontar "  + cantidad + " de " + material);
+    console.log("El stock actualizado es de " + resul);
+}
+else if (resul == 0) {
+    alert("Te quedas sin stock")
+}
+else {
+    alert("No tienes stock suficiente")
+} */
+
+
+
+    
+
+
+
+
+
+
+
+
---
+++ objetos.js
@@ -0,0 +1,26 @@
+debugger
+function objeto (material,medida,espesor,peso,valor,stock,) {
+    this.material = material
+    this.medida = medida
+    this.espesor = espesor
+    this.peso = peso
+    this.valor = valor
+    this.stock = stock
+    
+}
+
+const objeto1 = new objeto ("Galvanizado", "2000x1000", 2, 30, 50, 226)
+const objeto2 = new objeto ("Galvanizado", "3000x1500", 1.5, 63, 72, 1226)
+const objeto3 = new objeto ("Aluminio", "3000x1500", 1, 10, 35, 587)
+const objeto4 = new objeto ("Hierro", "2000x1000", 1, 32, 40, 698)
+const objeto5 = new objeto ("Hierro", "2500x1250", 1.5, 48, 62, 748)
+const objeto6 = new objeto ("Latón", "2000x1000", 1.5, 24 , 23, 37)
+
+
+function stockFinal(){
+    let cantidad = parseInt(prompt("ingrese la cantidad de material que se va a consumir"))
+    let resultado = obejto4.stock - cantidad
+    console.log("El stock actualizado es de " + resultado);
+}
+
+stockFinal()
